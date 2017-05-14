80550c5879663f82c62264470e872d2ad1e174cb
yewsiang <tangys108@gmail.com>
2017-05-14 19:28:41 +0800

    Assignment 1.Q3 (Softmax) done

--- CS231n/Assignment/assignment1/cs231n/classifiers/softmax.py
+++ CS231n/Assignment/assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,50 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+
+  # Shapes
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+
+  for i in xrange(num_train):
+    X_i = X[i]
+    scores = np.dot(X_i, W)
+
+    # Modify the values to prevent numerical instability due to large values
+    normalization_term = np.max(scores)
+    scores -= normalization_term
+
+    scores = np.exp(scores)
+    correct_class_score = scores[y[i]]
+
+    # Loss: L = -log (e^(s_yi) / sum_j (e^(s_j)))
+    loss += -np.log(correct_class_score / np.sum(scores))
+
+    # Gradient
+    # (Derivation)
+    # L = -XW_yi + log(sum_j(e^{XW_j}))
+    #
+    # (Formula)
+    # d(log_e( f(x)) )/dx = f'(x) / f(x)
+    #
+    # dL/dW_yi = -X + Xe^{XW_yi} / sum_j(e^{XW_j}) 
+    #          =   X(  e^{XW_yi} / sum_j(e^{XW_j}) - 1)
+    # dL/dW_j  =      Xe^{XW_j}  / sum_j(e^{XW_j}) 
+    #          =   X(  e^{XW_j}  / sum_j(e^{XW_j}) - 0)
+    # Therefore,
+    # dL/dW_k  =   X(  e^{XW_k}  / sum_j(e^{XW_j}) - (k == correct_class))
+    #
+    for j in xrange(num_classes):
+      dW[:,j] += X_i * (scores[j] / np.sum(scores) - (j == y[i]))
+
+  # Regularization
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg * W
+
+  # Average
+  loss /= num_train
+  dW /= num_train
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +96,46 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  
+  # Shapes
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+
+  scores = np.dot(X, W)
+
+  # Modify the values to prevent numerical instability due to large values
+  normalization_term = np.max(scores, axis=1)
+  normalization_term = np.tile(normalization_term, (num_classes, 1)).T
+  scores -= normalization_term
+  scores = np.exp(scores)
+  correct_class_scores = scores[range(0, num_train), y]
+
+  # Loss: L = -log (e^(s_yi) / sum_j (e^(s_j)))
+  loss = np.sum(-np.log(correct_class_scores / np.sum(scores, axis=1)))
+
+  # Gradient
+  # For each class j: dW[:,j] += X_i * (scores[j] / np.sum(scores) - (j == y[i]))
+  # Segment: (scores[j] / np.sum(scores)) 
+  softmax = np.divide(scores, np.tile(np.sum(scores, axis=1), (num_classes, 1)).T)
+
+  # Segment: (j == y[i])
+  correct_class = np.zeros(softmax.shape)
+  correct_class[range(0, correct_class.shape[0]),y] = 1
+
+  # Segment: (scores[j] / np.sum(scores) - (j == y[i]))
+  intermediate = softmax - correct_class
+
+  # Final
+  dW = np.dot(X.T, intermediate)
+
+  # Regularization
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg * W
+
+  # Average
+  loss /= num_train
+  dW /= num_train
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
